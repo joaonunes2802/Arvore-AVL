6b02c56967e7e65a6bdc4b72caac7506fabb78c8
João Pedro Nunes <nunesjoao@discente.ufg.br>
2022-08-13 16:13:57 -0300

    Funções criadas

--- AVL.c
+++ AVL.c
@@ -3,44 +3,72 @@
 
 typedef struct no{
     int conteudo;
-    struct no *pai;
     struct no *filhoEsq;
     struct no *filhoDir;
 }elemento;
 
-void inserir(elemento *raiz, int dado){
-    elemento *p = NULL, *q = NULL;
-    int aux = 0;
-    p = raiz;
-    do{
-        if(dado > p->conteudo && p->filhoDir == NULL){
-            q = (elemento *)malloc(sizeof(elemento));
-            q->conteudo = dado;
-            q->filhoDir = NULL;
-            q->filhoEsq = NULL;
-            q->pai = p;
-            p->filhoDir = q;
-            aux = 1;
-        }else if (dado < p->conteudo && p->filhoEsq == NULL){
-            q = (elemento *)malloc(sizeof(elemento));
-            q->conteudo = dado;
-            q->filhoDir = NULL;
-            q->filhoEsq = NULL;
-            q->pai = p;
-            p->filhoEsq = q;
-            aux = 1;
-        }else if(dado > p->conteudo && p->filhoDir != NULL){
-            inserir(p->filhoDir, dado);
-            aux = 1;
-        }else if(dado < p->conteudo && p->filhoEsq != NULL){
-            inserir(p->filhoEsq, dado);
-            aux = 1;
-        }
-    }while(aux != 1);
+void inserir(elemento **raiz, int dado){
+    if(*raiz == NULL){
+        *raiz = (elemento *) malloc(sizeof(elemento));
+        (*raiz)->conteudo = dado;
+        (*raiz)->filhoDir = NULL;
+        (*raiz)->filhoEsq = NULL;
+    }else if(dado > (*raiz)->conteudo){
+        inserir(&((*raiz)->filhoDir), dado);
+    }else if(dado <= (*raiz)->conteudo){
+        inserir(&((*raiz)->filhoEsq), dado);
+    }
 }
-int calculAlturaEsq(elemento *raiz); 
-void rotDir();
-void rotEsq();
+int calculAlturaEsq(elemento *raiz){
+    elemento *p;
+    p = raiz->filhoEsq;
+    if(p == NULL){
+        return -1;
+    }else{
+        int esq = calculAlturaEsq(p->filhoEsq);
+        int dir = calculAlturaEsq(p->filhoDir);
+        if(esq > dir){
+            return esq + 1;
+        }else{
+            return dir + 1;
+        }
+    }
+}
+
+int calculAlturaDir(elemento *raiz){
+    elemento *p;
+    p = raiz->filhoDir;
+    if (p == NULL){
+        return -1;
+    }else{
+        int esq = calculAlturaEsq(p->filhoEsq);
+        int dir = calculAlturaEsq(p->filhoDir);
+        if (esq > dir){
+            return esq + 1;
+        }else{
+            return dir + 1;
+        }
+    }
+}
+
+void rotDir(elemento *no){
+    elemento *p = NULL, *aux = NULL;
+    p = no->filhoEsq;
+    aux = p->filhoDir;
+    p->filhoDir = no;
+    no->filhoEsq = aux;
+    no = p;
+}
+
+void rotEsq(elemento *no){
+    elemento *p =   NULL, *aux = NULL;
+    p = no->filhoDir;
+    aux = p->filhoEsq;
+    p->filhoEsq = no;
+    no->filhoDir = aux;
+    no = p;
+}
+
 
 int main(){
     return 0;
