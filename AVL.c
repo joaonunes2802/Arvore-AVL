716c7157f2d415d2aab89f1f7b3aece552d8cb45
João Pedro Nunes <nunesjoao@discente.ufg.br>
2022-08-14 17:00:11 -0300

    -

--- AVL.c
+++ AVL.c
@@ -111,7 +111,7 @@ void inserir(elemento **raiz, int dado){
         inserir(&((*raiz)->filhoEsq), dado);
     }
     
-    int fator = fb(*raiz);
+    /*int fator = fb(*raiz);
     
     if (fator > 1 && dado < (*raiz)->filhoEsq->conteudo)
         rotDir((raiz));
@@ -125,17 +125,132 @@ void inserir(elemento **raiz, int dado){
 
     else if (fator < -1 && dado < (*raiz)->filhoDir->conteudo) {
         rotDirEsq(*raiz);
-  }
+  }*/
 
 }
 
-void preOrder(elemento *p)
+void preOrderBalanceamento(elemento *raiz)
+{
+    if (raiz != NULL)
+    {
+        if(((fb(raiz->filhoDir) == 1 || fb(raiz->filhoDir) == -1) && (fb(raiz->filhoEsq) == 1 || fb(raiz->filhoEsq) == -1))){
+            int fator = fb(raiz);
+            //int fator2 = fb(raiz->filhoEsq);
+            //int fator3 = fb(raiz->filhoDir);
+            if(fator >= 2 && raiz->filhoDir == NULL){
+                int fator2 = fb(raiz->filhoEsq);
+                if (fator2 == 1){
+                    rotDir(raiz);
+                }else if(fator2 == -1){
+                    rotEsq(raiz->filhoEsq);
+                    rotDir(raiz);
+                }
+            }
+            else if (fator <= -2 && raiz->filhoDir == NULL){
+                int fator2 = fb(raiz->filhoEsq);
+                if (fator2 == 1)
+                {
+                    rotDir(raiz->filhoEsq);
+                    rotEsq(raiz);
+                }
+                else if (fator2 == -1)
+                {
+                    rotEsq(raiz);
+                }
+            }
+            else if (fator >= 2 && raiz->filhoEsq == NULL){
+                int fator3 = fb(raiz->filhoDir);
+                if (fator3 == 1)
+                {
+                    rotDir(raiz);
+                }
+                else if (fator3 == -1)
+                {
+                    rotEsq(raiz->filhoDir);
+                    rotDir(raiz);
+                }
+            }
+            else if (fator <= -2 && raiz->filhoEsq == NULL){
+                int fator3 = fb(raiz->filhoDir);
+                if (fator3 == 1)
+                {
+                    rotDir(raiz->filhoDir);
+                    rotEsq(raiz);
+                }
+                else if (fator3 == -1)
+                {
+                    rotEsq(raiz);
+                }
+            }else if(fator >= 2 && (raiz->filhoDir != NULL && raiz->filhoEsq != NULL)){
+                int fator3 = fb(raiz->filhoDir);
+                int fator2 = fb(raiz->filhoEsq);
+                if (fator2 == 1 && fator3 == 1){
+                    rotDir(raiz);
+                }
+                else if (fator2 == -1){
+                    rotEsq(raiz->filhoEsq);
+                    rotDir(raiz);
+                }
+                else if (fator3 == -1){
+                    rotEsq(raiz->filhoDir);
+                    rotDir(raiz);
+                }
+            }
+            else if (fator <= -2 && (raiz->filhoDir != NULL && raiz->filhoEsq != NULL)){
+                int fator3 = fb(raiz->filhoDir);
+                int fator2 = fb(raiz->filhoEsq);
+                if (fator2 == -1 && fator3 == -1){
+                    rotEsq(raiz);
+                }else if(fator2 == 1){
+                    rotDir(raiz->filhoEsq);
+                    rotEsq(raiz);
+                }else if(fator3 == 1){
+                    rotDir(raiz->filhoDir);
+                    rotEsq(raiz);
+                }
+            }
+                /*if (fator >= 2 && (fator2 == 1 && fator3 == 1))
+                {
+                    rotDir(raiz);
+                }
+                else if (fator <= -2 && (fator2 == -1 && fator3 == -1))
+                {
+                    rotEsq(raiz);
+                }
+                else if (fator >= 2 && fator2 == -1)
+                {
+                    rotEsq(raiz->filhoEsq);
+                    rotDir(raiz);
+                }
+                else if (fator <= -2 && fator2 == 1)
+                {
+                    rotDir(raiz->filhoEsq);
+                    rotEsq(raiz);
+                }
+                else if (fator >= 2 && fator3 == -1)
+                {
+                    rotEsq(raiz->filhoDir);
+                    rotDir(raiz);
+                }
+                else if (fator <= -2 && fator3 == 1)
+                {
+                    rotDir(raiz->filhoDir);
+                    rotEsq(raiz);
+                }*/
+        }else{
+            preOrderBalanceamento(raiz->filhoEsq);
+            preOrderBalanceamento(raiz->filhoDir);
+        }
+    }
+}
+
+void preOrderImprimindo(elemento *p)
 {
     if (p != NULL)
     {
         printf("%d\n", p->conteudo);
-        preOrder(p->filhoEsq);
-        preOrder(p->filhoDir);
+        preOrderImprimindo(p->filhoEsq);
+        preOrderImprimindo(p->filhoDir);
     }
 }
 
@@ -143,7 +258,7 @@ int main(){
     elemento *raiz = NULL, *raiz2 = NULL, *raiz3 = NULL;
     int n[6] = {8,4,10,9,15,12};
     int n2[6] = {8,4,10,2,6,5};
-    int n3[6]={8,2,15,9,20,17};
+    //int n3[6]={8,2,15,9,20,17};
     for(int i = 0 ; i < 7; i++){
         inserir(&raiz, n[i]);
     }
@@ -153,18 +268,17 @@ int main(){
         inserir(&raiz2, n2[i]);
     }
 
-    for (int i = 0; i < 7; i++)
+    /*for (int i = 0; i < 7; i++)
     {
         inserir(&raiz3, n3[i]);
-    }
+    }*/
 
-    printf("\nImprimindo a arvore n já balanceada\n");
-    preOrder(raiz);
+    printf("\nImprimindo a arvore n ja balanceada\n");
+    preOrderBalanceamento(raiz);
+    preOrderImprimindo(raiz);
 
-    printf("\nImprimindo a arvore n2 já balanceada\n");
-    preOrder(raiz2);
-
-    printf("\nImprimindo a arvore n3 já balanceada\n");
-    preOrder(raiz3);
+    printf("\nImprimindo a arvore n2 ja balanceada\n");
+    preOrderBalanceamento(raiz2);
+    preOrderImprimindo(raiz2);
     return 0;
 }
